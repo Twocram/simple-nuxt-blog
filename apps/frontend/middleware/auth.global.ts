5e4c2e2fb8819c7f259f27943930b45a0643c2fa
Artem <56472854+Twocram@users.noreply.github.com>
2025-01-13 14:44:17 +0500

    fix: hydration error after redirect

--- apps/frontend/middleware/auth.global.ts
+++ apps/frontend/middleware/auth.global.ts
@@ -1,17 +1,17 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (import.meta.client) {
-    const token = window.localStorage.getItem('token');
+export default defineNuxtRouteMiddleware(async (to) => {
+  const token = import.meta.client
+    ? window.localStorage.getItem('token')
+    : null;
 
-    if (to.path !== '/auth/login' && to.path !== '/auth/register') {
-      if (!token) {
-        return navigateTo('/auth/login');
-      }
+  if (to.path !== '/auth/login' && to.path !== '/auth/register') {
+    if (!token) {
+      return navigateTo('/auth/login');
     }
+  }
 
-    if (to.path === '/auth/login' || to.path === '/auth/register') {
-      if (token) {
-        return navigateTo('/');
-      }
+  if (to.path === '/auth/login' || to.path === '/auth/register') {
+    if (token) {
+      return navigateTo('/');
     }
   }
 });
